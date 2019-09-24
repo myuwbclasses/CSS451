6802807b1a43a70ed4fda884c8adb55f5ad3a61b
kelvinhsung <ksung@u.washington.edu>
2019-09-23 18:24:58 -0700

    Almost done with Wk4

---
+++ ClassExamples/Week4/Week4.Examples/2.TRS/Assets/Source/Model/TheWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheWorld : MonoBehaviour  {
+   
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
