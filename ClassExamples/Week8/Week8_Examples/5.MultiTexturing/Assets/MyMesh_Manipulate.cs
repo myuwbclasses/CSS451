6e42ce81c217c47f9279cb25a1fae9a84f059e22
kelvinhsung <ksung@uw.edu>
2019-11-13 07:43:12 -0800

    Weeks 7 to 9

---
+++ ClassExamples/Week8/Week8_Examples/5.MultiTexturing/Assets/MyMesh_Manipulate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class MyMesh : MonoBehaviour {
+
+    GameObject[] mControllers;
+
+    void InitControllers(Vector3[] v)
+    {
+        mControllers = new GameObject[v.Length];
+        for (int i =0; i<v.Length; i++ )
+        {
+            mControllers[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            mControllers[i].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            mControllers[i].transform.localPosition = v[i];
+            mControllers[i].transform.parent = this.transform;
+        }
+    }
+}
---
+++ ClassExamples/Week9/Week9_Examples/3.Transparency/Assets/Source/Prefab Support/Line/UILineSegment.cs
@@ -0,0 +1,38 @@
+﻿#define OurOwnRotation
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    // Assume hanging on a cylinder
+public class UILineSegment : LineSegment {
+    public LineEndPt mEndP1;
+    public LineEndPt mEndP2;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Assert(mEndP1 != null);
+        Debug.Assert(mEndP2 != null);
+        base.SetEndPoints(mEndP1.GetPosition(), mEndP2.GetPosition());
+        ComputeLineDetails();
+    }
+
+    // setters
+    public override void SetEndPoints(Vector3 p1, Vector3 P2)
+    {
+        base.SetEndPoints(p1, P2);
+        mEndP1.SetPosition(p1);
+        mEndP2.SetPosition(P2);
+    }
+
+    public bool SetEndPoint(string onWall, Vector3 p)
+    {
+        bool hit = (mEndP1.SetPosition(onWall, p) || mEndP2.SetPosition(onWall, p));
+        if (hit)
+        {
+            base.SetEndPoints(mEndP1.transform.localPosition, mEndP2.transform.localPosition);
+            ComputeLineDetails();
+        }
+        return hit;
+    }
+    
+}
