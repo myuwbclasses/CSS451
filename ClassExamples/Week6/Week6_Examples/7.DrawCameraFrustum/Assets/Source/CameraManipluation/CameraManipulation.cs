14ffa46c80fa2b85956fd2389e589cc2ce0b5c43
Kelvin Sung <ksung@uw.edu>
2022-11-16 14:39:24 -0800

    Update CameraManipulation.cs

--- ClassExamples/Week6/Week6_Examples/7.DrawCameraFrustum/Assets/Source/CameraManipluation/CameraManipulation.cs
+++ ClassExamples/Week6/Week6_Examples/7.DrawCameraFrustum/Assets/Source/CameraManipluation/CameraManipulation.cs
@@ -6,10 +6,6 @@ public partial class CameraManipulation : MonoBehaviour
     public Transform LookAt;
     public bool DrawFrustum = false; 
 
-    private float mMouseX = 0f;
-    private float mMouseY = 0f;
-    private const float kPixelToDegree = 0.1f;
-    private const float kPixelToDistant = 0.05f;
 
     // Use this for initialization
     void Start()
