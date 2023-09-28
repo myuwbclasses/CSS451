c637ab9f8a7e84843c6963a56417ec68db27a2ed
Kelvin Sung <ksung@uw.edu>
2023-09-28 13:46:52 -0700

    Week 2 ready

---
+++ ClassExamples/Week2/Week2-MVC-Example/2.MoreInvolved/HierarchicalModeling/MP5/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Windows.Forms;
+
+namespace ClassExample
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+
+    }
+#endif
+}
+
