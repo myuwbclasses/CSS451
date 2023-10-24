69a07ec647562eba9c27eebc3e1a23770c0e1bcc
Kelvin Sung <ksung@uw.edu>
2023-10-24 05:59:55 -0700

    Topic4 notes

---
+++ ClassExamples/Topic4-Matrices/1.AccessToTransforms/Assets/Source/UI Support/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System; // for assert
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // for GUI elements: Button, Toggle
+
+public partial class MainController : MonoBehaviour {
+
+    // reference to all UI elements in the Canvas
+    public Camera MainCamera = null;
+    public TheWorld TheWorld = null;
+    
+
+    // Use this for initialization
+    void Start() {
+        Debug.Assert(MainCamera != null);
+        Debug.Assert(TheWorld != null);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // ProcessMouseEvents();
+    }
+}
